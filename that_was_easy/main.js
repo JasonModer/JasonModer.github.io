b2846a33335031fe9ad33693051af2524dc5070e
Jason <jason.yuyang.wang@gmail.com>
2017-06-28 19:41:18 +0000

    Spacebar

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -8,7 +8,7 @@ $("#easy").on("click", sayThatWasEasy);
 $(document).keypress(delegateKeypress);
 
 function delegateKeypress(event) {
- if (event.charCode == 32) {
-  $("#easy").trigger("click");
+  if (event.charCode == 106) {
+    $("#easy").trigger("click");
   }
 }
